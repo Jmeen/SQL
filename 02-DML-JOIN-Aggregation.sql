c3fb64e78180d032eddcbd4e292eb787d1ece998
Jaemin.Lim <jmeeen@gmail.com>
2021-08-04 14:25:15 +0900

    Group & Aggregation

--- 02-DML-JOIN-Aggregation.sql
+++ 02-DML-JOIN-Aggregation.sql
@@ -92,7 +92,6 @@ FROM employees emp FULL OUTER JOIN departments dpt
 -- MANAGER의 EMPLOYEE ID
 -- MANAGER의 FIRST_NAME    
  
-
  SELECT * FROM employees; -- 107명
  -- 사원 정보
 SELECT emp.employee_id, emp.first_name, man.employee_id, man.first_name
@@ -111,3 +110,80 @@ SELECT emp.employee_id, emp.first_name, man.employee_id, man.first_name
 FROM employees emp LEFT OUTER JOIN employees man
 ON emp.manager_id = man.employee_id
 ORDER BY emp.employee_id;
+
+
+
+
+
+
+-----------------------------------------------------------
+-- 집계 함수
+-----------------------------------------------------------
+-- 여러 레코드로부터 데이터를 수집. 하나의 결과 행을 반환한다.
+-- count:갯수세기
+
+SELECT count(*) FROM employees; -- 특정 컬럼이 아닌 레코드의 갯수를 센다.
+
+SELECT count(commission_pct) FROM employees; -- 해당 컬럼이 null이 아닌 갯수
+SELECT count (*) FROM employees
+WHERE commission_pct IS NOT NULL;  -- 위 코드와 같은 의미
+
+-- SUM : 합계
+SELECT SUM(salary) FROM employees;
+
+-- AVG : 평균
+SELECT ROUND(AVG(salary),2) FROM employees;
+
+-- 사원들의 평균 커미션 비율
+SELECT ROUND(avg(commission_pct),2) FROM employees;  -- 22%
+SELECT ROUND(avg(nvl(commission_pct,0)),2) FROM employees;  -- 7%
+
+-- 최댓값/ 최솟값
+SELECT MAX(salary) 최대, MIN(salary) 최소, ROUND(AVG(salary)) 평균, MEDIAN(salary) 중앙값
+FROM employees;
+
+-- 일반적 오류
+SELECT department_id, Avg(salary)
+FROM employees; 
+
+-- 수정 : 집계함수
+SELECT department_id, ROUND(Avg(salary))
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
+
+-- 집계함수를 사용한 Select문의 컬럼 목록에는 GROUP BY에 참여한 필드, 집계함수만 올 수 있다.
+
+-- 부서별 평균 급여를출력
+-- 평균 급여가 7000이상인 부서만 뽑아보자.
+SELECT department_id, ROUND(Avg(salary))
+FROM employees
+WHERE AVG(salary)>7000
+GROUP BY department_id;  -- ERROR
+-- 집계 함수 실행 이전에 WHERE 절을 검사하기 때문에, 사용된 집계함수는 WHERE절에서 사용할 수 없다.
+
+SELECT department_id, ROUND(Avg(salary))
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary)>=7000
+ORDER BY department_id;
+
+-----------------------------------------------------------
+-- 분석 함수
+-----------------------------------------------------------
+
+-- ROLL UP ; 그룹핑된 결과에 대한 상세 요약을 제공하는 기능
+-- 일종의 ITEM total
+SELECT department_id, job_id, SUM(salary)
+FROM employees
+GROUP BY ROLLUP (department_id, job_id);
+
+-- CUBE 함수 : Corss Table에 대한 Summary를 함꼐 추출
+-- ROLLUP 함수에서 추출되는 Item total 과 함께 column total값을 함께 추출
+
+SELECT department_id, job_id, SUM(salary)
+FROM employees
+GROUP BY cube (department_id, job_id)
+--HAVING SUM(salary)>50000
+ORDER BY department_id;
+
